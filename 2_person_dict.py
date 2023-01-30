0a0311a4a6f3e4c33f59c05a4e3b7143e8c60fd6
Korbin Scott <korbin_scott1@baylor.edu>
2023-01-30 15:44:30 -0600

    1/30

--- 2_person_dict.py
+++ 2_person_dict.py
@@ -5,3 +5,19 @@ person["age"] = 51
 person["spouse"] = "Edna"
 person["children"] = ["Ralph", "Betty", "Joey"]
 person["pets"] = {"dog": "Fido", "cat": "Sox"}
+
+# print(person)
+
+# print out the name of the second child
+# print(person["children"][1])
+
+# print out the name of the cat
+# print(person["pets"]["cat"])
+
+# iterate through all children and print out each child
+# for key in person["children"]:
+    # print(key)
+
+# print out the pets in this format:
+# type of pet: dog name of pet: Fido
+print(f"type of pet: {person["pets"]["dog"]} ")
