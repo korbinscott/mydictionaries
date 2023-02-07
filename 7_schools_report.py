ce7f2651c7dd46c09a9d21aa4189a307add3ebfa
Korbin Scott <korbin_scott1@baylor.edu>
2023-02-06 22:50:11 -0600

    2/6

--- 7_schools_report.py
+++ 7_schools_report.py
@@ -11,9 +11,38 @@ Copy that info here:
 "NCAA/NAIA conference number football (IC2020)","130","Southeastern Conference"
 
 
-Display report for all universities that have a graduation rate for Women over 50%
+Display report for all universities that have a graduation rate for Women over 80%
 Display report for all universities that have a total price for in-state students living off campus over $50,000
 
 
 
 """
+
+import json
+
+infile = open("school_data.json", "r")
+
+schools = json.load(infile)
+conference_schools = [372, 108, 107, 130]
+
+""" for school in schools:
+    if school["NCAA"]["NAIA conference number football (IC2020)"] in conference_schools:
+        if school["Graduation rate  women (DRVGR2020)"] > 80:
+            print(f"University Name: {school['instnm']}")
+            print(
+                f"Graduation Rate for Women: {school['Graduation rate  women (DRVGR2020)']}"
+            )
+            print()
+            print() """
+
+
+for school in schools:
+    school_price = school[
+        "Total price for in-state students living off campus (not with family)  2020-21 (DRVIC2020)"
+    ]
+    if school["NCAA"]["NAIA conference number football (IC2020)"] in conference_schools:
+        if int(school_price) > 50000:
+            print(f"University Name: {school['instnm']}")
+            print(
+                f"Total price for in-state student living off campus: {school['Total price for in-state students living off campus (not with family)  2020-21 (DRVIC2020)']}"
+            )
